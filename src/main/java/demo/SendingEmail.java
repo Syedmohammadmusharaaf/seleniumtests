dbe6e629636723fe93f3419e8e456c388ec53717
SyedMohammadMusharaaf <syedmdmusharaaf@gmail.com>
2025-04-17 20:54:27 +0530

    sendingEmail class is added

---
+++ src/main/java/demo/SendingEmail.java
@@ -0,0 +1,65 @@
+package demo;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class SendingEmail {
+    private static final Logger log = LoggerFactory.getLogger(SendingEmail.class);
+
+    public static void main(String[] args) {
+        String  senderEmail ="mohammadmusharaafsyed@gmail.com";
+        String  appPassword ="elnpiounqnzbsytz";
+        String receiverEmail ="mohammadmusharaafsyed@gmail.com";
+
+        //SMTP CONFIGURATIONS
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.port", "587");
+
+        // Create session with Authentication
+        Session session = Session.getInstance(prop, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(senderEmail, appPassword);
+            }
+        });
+        session.setDebug(true);
+
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(senderEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(receiverEmail));
+            message.setSubject("Test Results from  Automation QA Team");
+//            message.setText("from QA team \n regards \n Syed Musharaaf");
+
+            //setting text part from MimeBodyPart class
+            MimeBodyPart textPart = new MimeBodyPart();
+            textPart.setText("from QA team \\n regards \\n Syed Musharaaf");
+
+            MimeBodyPart attachmentPart = new MimeBodyPart();
+            attachmentPart.attachFile(System.getProperty("user.dir")+"/reports/Extent.html");
+
+            MimeMultipart combine = new MimeMultipart();
+            combine.addBodyPart(textPart);
+            combine.addBodyPart(attachmentPart);
+            message.setContent(combine);
+
+            Transport.send(message);
+            System.out.println("email sent successfully");
+            System.out.println("email is sent");
+
+
+        } catch (Exception e) {
+           e.printStackTrace();
+        }
+    }
+}
